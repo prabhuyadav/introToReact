e8676b9ff060e806e867db1cd6d0bf0cb97833e8
prabhu yadav p.s <prabhuyadav007.p.y@gmail.com>
2021-04-07 06:58:34 +0530

    Adding Style to JSX Added detailed explanation in notes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const myName = "Prabhu";
+const currentYear = new Date().getFullYear();
 
 const htmlElement = (
   <div>
-    <h1>Hello React</h1>
+    <h1 className="heading">Hello React</h1>
     <p>This is React Introduction. My name is {myName}</p>
+    <p>Copyright {currentYear}</p>
   </div>
 );
 // Rendering this htmlElement
