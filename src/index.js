29a4444047a2bfbe3462d7b88f49290ab18dffe2
prabhu yadav p.s <prabhuyadav007.p.y@gmail.com>
2021-04-07 06:23:16 +0530

    Including js in html in JSX .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const myName = "Prabhu";
+
 const htmlElement = (
   <div>
     <h1>Hello React</h1>
-    <p>This is React Introduction</p>
+    <p>This is React Introduction. My name is {myName}</p>
   </div>
 );
-
+// Rendering this htmlElement
 ReactDOM.render(htmlElement, document.getElementById("root"));
