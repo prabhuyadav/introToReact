4ecd3f55d35eafd017db6362738b6fcd1e842167
prabhu yadav p.s <prabhuyadav007.p.y@gmail.com>
2021-04-07 08:11:51 +0530

    Inline Styles in JSX

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import ReactDOM from "react-dom";
 const myName = "Prabhu";
 const currentYear = new Date().getFullYear();
 
+const customStyle = {
+  color: "violet",
+  fontSize: "15px",
+  padding: "5px 10px 5px 10px",
+  border: "1px solid black"
+};
+
+// now we can change the customStyle properties dynamically through code.
+
+customStyle.fontSize = "16px";
+
 const htmlElement = (
   <div>
     <h1 className="heading">Hello React</h1>
     <p>This is React Introduction. My name is {myName}</p>
-    <p>Copyright {currentYear}</p>
+    <span style={customStyle}>Copyright {currentYear}</span>
   </div>
 );
 // Rendering this htmlElement
